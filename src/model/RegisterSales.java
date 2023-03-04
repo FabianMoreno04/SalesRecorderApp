9fc22a4446a2d9d89075e5a25e2f4e02c45571e6
FabianMoreno04 <126001342+FabianMoreno04@users.noreply.github.com>
2023-03-03 22:07:49 -0500

    Add files via upload

--- src/model/RegisterSales.java
+++ src/model/RegisterSales.java
@@ -2,4 +2,65 @@ package model;
 
 public class RegisterSales {
 
+	private int id;
+	private String InvoiceDate;
+	private String nameCustomer;
+	private int discount;
+	private double valueTotal;
+
+	public RegisterSales(int id, String invoiceDate, String nameCustomer, int discount, double valueTotal) {
+		this.id = id;
+		this.InvoiceDate = invoiceDate;
+		this.nameCustomer = nameCustomer;
+		this.discount = discount;
+		this.valueTotal = valueTotal;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getInvoiceDate() {
+		return InvoiceDate;
+	}
+
+	public void setInvoiceDate(String invoiceDate) {
+		InvoiceDate = invoiceDate;
+	}
+
+	public String getNameCustomer() {
+		return nameCustomer;
+	}
+
+	public void setNameCustomer(String nameCustomer) {
+		this.nameCustomer = nameCustomer;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public double getValueTotal() {
+		return valueTotal;
+	}
+
+	public void setValueTotal(double valueTotal) {
+		this.valueTotal = valueTotal;
+	}
+	
+	@Override
+	public String toString() {
+	   return id + ", " + InvoiceDate + ", " + nameCustomer + ", " + discount + ", " + valueTotal + ".";
+	}
+	
+	
+	
 }
